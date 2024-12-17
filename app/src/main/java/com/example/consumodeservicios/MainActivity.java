6bbb4b597d1de5528a7ea28206ae687560765c3d
MateoMendez07 <henry.mendez549@ist17dejulio.edu.ec>
2024-12-17 00:43:30 -0500

    Diseño de las vistas para las figuras geometricas

--- app/src/main/java/com/example/consumodeservicios/MainActivity.java
+++ app/src/main/java/com/example/consumodeservicios/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.consumodeservicios;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.sax.StartElementListener;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.lblTitulo), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnPrimerServicio = findViewById(R.id.btnPrimerServicio);
+        Button btnSegundoServicio = findViewById(R.id.btnSegundoServicio);
+        Button btnTercerServicio = findViewById(R.id.btnTercerServicio);
+        Button btnCuartoServicio = findViewById(R.id.btnCuartoServicio);
+        Button btnQuintoServicio = findViewById(R.id.btnQuintoServicio);
+        Button btnSextoServicio = findViewById(R.id.btnSextoServicio);
+        Button btnSeptimoServicio = findViewById(R.id.btnSeptimoServicio);
+        btnPrimerServicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PrimerServicio.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSegundoServicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SegundoServicio.class);
+                startActivity(intent);
+            }
+        });
+
+        btnTercerServicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TercerServicio.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCuartoServicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CuartoServicio.class);
+                startActivity(intent);
+            }
+        });
+
+        btnQuintoServicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, QuintoServicio.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSextoServicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SextoServicio.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSeptimoServicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SeptimoServicio.class);
+                startActivity(intent);
+            }
         });
     }
 }
